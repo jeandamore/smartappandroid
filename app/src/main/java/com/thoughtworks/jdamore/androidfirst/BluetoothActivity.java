ccd1ca45dcaded13b20a845a2150b19c0dc5f304
Sarah Nelson <Sarah@Sarahs-MacBook-Pro.local>
2015-09-01 10:50:34 +1000

    Checks for paired devices and displays them at the top of the list

--- app/src/main/java/com/thoughtworks/jdamore/androidfirst/BluetoothActivity.java
+++ app/src/main/java/com/thoughtworks/jdamore/androidfirst/BluetoothActivity.java
@@ -1,20 +1,20 @@
 package com.thoughtworks.jdamore.androidfirst;
 
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import java.util.ArrayList;
+import java.util.Set;
 
 public class BluetoothActivity extends AppCompatActivity {
 
@@ -30,13 +30,59 @@ public class BluetoothActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listView);
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // Check if device does not support Bluetooth
+
+        checkIfDeviceDoesNotSupportBluetooth();
+
+        checkIfBluetoothIsNotEnabled();
+
+        checkForPairedDevices();
+
         mBluetoothAdapter.startDiscovery();
 
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         registerReceiver(mReceiver, filter);
-
     }
 
+    private void checkIfBluetoothIsNotEnabled() {
+        if (!mBluetoothAdapter.isEnabled()) {
+            int REQUEST_ENABLE_BT = 1;
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    private void checkIfDeviceDoesNotSupportBluetooth() {
+        if (mBluetoothAdapter == null) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setMessage("Device does not support Bluetooth");
+            dialog.setCancelable(true);
+            dialog.setPositiveButton("Okay",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alert1 = dialog.create();
+            alert1.show();
+        }
+    }
+
+    private void checkForPairedDevices() {
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                mDeviceList.add("Paired device" + device.getName() + "\n" + device.getAddress());
+            }
+        } else {
+            mDeviceList.add("No paired devices found");
+        }
+    }
 
     @Override
     protected void onDestroy() {
